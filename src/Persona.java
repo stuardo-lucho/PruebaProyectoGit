a1cdb1d2c57261cb366d6ef2fb3a8ca9d0cb3cce
AlonsoGuevara15 <a20160679@pucp.edu.pe>
2020-10-13 20:32:58 -0500

    dni en persona

--- src/Persona.java
+++ src/Persona.java
@@ -1,2 +1,11 @@
 public class Persona {
+    private String dni;
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
 }
